e41848b9f6f3d7514af26b57805c9fef1ea7998c
Rebecca Turner <637275@gmail.com>
2018-08-04 14:09:00 -0400

    ???

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate regex;
 
 use std::process::Command;
+use std::collections::HashSet;
 use regex::Regex;
 
 fn man(cmd: &str) -> String {
@@ -16,15 +17,24 @@ fn args(cmd: &str) -> Vec<String> {
     // an argument is space, a dash, alphanumerics, and space
     let arg_re = Regex::new("\\s-[[:alnum:]-]+\\s").unwrap();
     // get the man page text
-    let man_page: String = man(cmd);
+    let man_page = man(cmd);
     // collect arguments in the man page into a vec and return them
-    arg_re.find_iter(&man_page).map(
-        |m| String::from(m.as_str()).trim().to_string()
-        ).collect::<Vec<String>>()
+    let arg_set = arg_re.find_iter(&man_page).map(
+        // hell
+        |m| m.as_str()
+        ).collect::<HashSet<&str>>();
+    arg_set.iter()
+        .map(|m| m.to_string().trim().to_string())
+        .collect::<Vec<String>>()
 }
 
+//fn fake_invocation(cmd: &str, args: Vec<String>) {
+//}
+
 fn main() {
-    for arg in args("gcc") {
+    let args = args("gcc");
+    for arg in &args {
         println!("{}", arg);
     }
+    println!("{} args", args.len());
 }
