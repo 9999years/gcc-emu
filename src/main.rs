ba9fd1b5108eb1e0fd5442a396bdd06f8c9f28d3
Rebecca Turner <637275@gmail.com>
2018-08-03 23:55:48 -0400

    argument getting works!!

--- src/main.rs
+++ src/main.rs
@@ -12,17 +12,19 @@ fn man(cmd: &str) -> String {
 }
 
 // returns arguments listed in the man page for `cmd`
-fn args(cmd: &str) -> Vec<&str> {
+fn args(cmd: &str) -> Vec<String> {
     // an argument is space, a dash, alphanumerics, and space
-    let arg_re = Regex::new("\\s-[[:alnum:]-]\\s").unwrap();
+    let arg_re = Regex::new("\\s-[[:alnum:]-]+\\s").unwrap();
     // get the man page text
     let man_page: String = man(cmd);
     // collect arguments in the man page into a vec and return them
     arg_re.find_iter(&man_page).map(
-        |m| m.as_str()
-        ).collect::<Vec<&str>>()
+        |m| String::from(m.as_str()).trim().to_string()
+        ).collect::<Vec<String>>()
 }
 
 fn main() {
-    args("gcc");
+    for arg in args("gcc") {
+        println!("{}", arg);
+    }
 }
